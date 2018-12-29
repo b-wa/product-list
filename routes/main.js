729aa1bc71dfe0d4f3a1a6aefcf0ba28667c212b
Betty <bmay.wanjiru@gmail.com>
2018-12-29 17:35:23 -0500

    Part one of eval almost complete. All endpoints are working except the one for adding a review

--- routes/main.js
+++ routes/main.js
@@ -33,11 +33,31 @@ router.get("/generate-fake-data", (req, res, next) => {
 
 
 //implementing pagination
-router.get("/products", (req, res, next) => {
+router.get("/products?:page?:price", (req, res, next) => {
     const perPage = 9
     const page = req.query.page || 1
+    let category = {}
+    let price = {}
+
+    if(req.query.category) {
+        category = {"category": req.query.category};
+    } else {
+        options = null;
+    }
+
+    if(req.query.price) {
+        if (req.query.price ==='lowest') {
+            price = {"price": 1}
+        } else if (req.query.price ==='highest') {
+            price = {"price": -1}
+        } else {
+            options = null;
+        }
+    }
+
     Product
-        .find({})
+        .find(category)
+        .sort(price)
         .skip((perPage * page) - perPage)
         .limit(perPage)
         .exec((err, products) => {
@@ -49,27 +69,32 @@ router.get("/products", (req, res, next) => {
         })
     })
 
-//getting the products by product id
-router.get("/products/:product", (req, res) => {
+//return a specific product by it's id
+router.get("/products/:product", (req, res, next) => {
     const {_id} = req.params.product
     Product.findById(_id, (err, product) => {
-        if (err) throw err;
-        //console.log(product)
-    });
-});
+        if (err) {
+            return next (err);
+        }
+        res.send(product);
+    })
+})
 
-//get the all the product reviews
+
+//get the all the product reviews but limited to 40 at a time
 router.get("/reviews", (req, res) => {
-    Review.find((err, reviews) => {
-        Review.populate(reviews, {path: 'reviews'}, (err, data) => {
-            if (err) throw err;
-            console.log(data);
-            res.send(reviews);
-        });
+    Review
+        .find({})
+        .limit(40)
+        .exec((err, reviews) => {
+            Review.count().exec((err, count) => {
+                if (err) return next(err)
+                res.send(reviews)
+            })
+        })
     });
-});
 
-//add new products to the data base
+//add new products to the database
 router.post("/products", (req, res) => {
     let newProduct = new Product({
         category: req.body.category,
@@ -85,7 +110,7 @@ router.post("/products", (req, res) => {
     res.send(newProduct);
 })
 
-//add new reviews for a particular(single) product
+//add new reviews for a specific product
 router.post("/:product/reviews", (req, res) => {
     const productId = req.params.product;
     let newReview = new Review({
@@ -99,22 +124,25 @@ router.post("/:product/reviews", (req, res) => {
         product.reviews.push(newReview);
         res.send(product)
     }).exec()
-
-   
-
-
 });
 
-// //delete a specific product
-// router.delete("/products/:product", (req, res) => {
+//delete a product by its id
+router.delete("/products/:product", (req, res) => {
+    const {_id} = req.params.product 
+    Product.findByIdAndRemove(_id, (err, product) => {
+        if (err) throw err;
+        res.status(200).send("Product was successfully deleted");
+    });
+});
 
-// });
-
-// //delete any review
-// router.delete("/reviews/:review", (req, res) => {
-// });
-
-
+//delete a review by it's id
+router.delete("/reviews/:review", (req, res) => {
+    const {_id} = req.params.review
+    Review.findByIdAndDelete(_id, (err, review) => {
+        if (err) throw err;
+        res.status(200).send("Review successfully deleted ");
+    })
+ });
 
 
 module.exports = router
